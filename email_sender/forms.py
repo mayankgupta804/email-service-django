6c96568a310afcc15b9e04ab22f9fcb890d90ea7
mayankgupta804 <mayank.gupta804@gmail.com>
2019-04-05 12:33:46 +0530

    Create models, views, templates and urls for email_sender app

---
+++ email_sender/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from .models import Email
+
+class EmailForm(forms.ModelForm):
+    
+    class Meta:
+        model = Email
+        fields = ('email_id', 'subject', 'body')
